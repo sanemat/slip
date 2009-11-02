390c7c9babf9cf04f842f6cd245e5507c1965dc7
sanemat <o.gata.ken@gmail.com>
2009-11-03 02:27:46 +0900

    url captured
    url captured

--- slip.rb
+++ slip.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
 require 'sinatra'
+require 'open-uri'
 
 get '/' do
   "world of markdown to presentation!"
 end
 
-get '/:url' do
-   "#{params[:url]}"
+get %r{/(.+)} do
+  "url is #{params[:captures]}"
 end
