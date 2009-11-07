741ed40a43d85458b17e93e4cf521ea045c8deca
sanemat <o.gata.ken@gmail.com>
2009-11-08 00:35:29 +0900

    parse markdown file from web
    parse markdown file from web

--- slip.rb
+++ slip.rb
@@ -1,11 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 require 'open-uri'
+require 'slidedown'
 
 get '/' do
   "world of markdown to presentation!"
 end
 
+class Slip < SlideDown
+  def self.render(source_path, template = "default")
+    if source_path
+      slideshow = new(open(source_path).read)
+      slideshow.render(template)
+    end
+  end
+end
+
 get %r{/(.+)} do
-  "url is #{params[:captures]}"
+  "#{Slip::render(params[:captures].to_s)}"
 end
